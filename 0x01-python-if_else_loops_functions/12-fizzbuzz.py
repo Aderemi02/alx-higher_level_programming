2d5f50dadcafb3037a53ecd0536ca615807a46e0
Aderemi <omoremilekun@gmail.com>
2023-06-10 08:42:32 -0700

    printing number from 1 to 100

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1,101):
+        if i == 3  or i % 3 == 0:
+            print(f"{Fizz}", end="")
+        elif i == 5 or i % 5 == 0:
+            print(f"{Buzz}", end="")
+        elif i % 5 == 0 and i % 3 == 0:
+            print(f"{FizzBuzz}", end="")
