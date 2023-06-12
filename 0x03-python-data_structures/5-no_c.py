a33ae5d95b2a0536b6166871ee64e7ef298483f6
Aderemi <omoremilekun@gmail.com>
2023-06-12 13:57:48 -0700

    omitting c and C in a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(my_string)
-    if i == 'c' or 'C':
-        break
+    new = [i for i in my_string if i != 'c' and i != 'C')
+    my_string = "".join(new)
     return my_string
