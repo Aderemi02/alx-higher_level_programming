f594d15d191b48f9ed3583521fad4a047d4c5c1c
Aderemi <omoremilekun@gmail.com>
2023-06-12 10:25:35 -0700

    function to add a new list to a sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * insert_node - inserting a new node to a list
+ * @head: the beginning of the list
+ * @number: value to be added to the list
+ * Return: the address to the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *Nnode, *temp = *head;
+
+	Nnode = malloc(sizeof(list_int));
+	if (Nnode == NULL)
+		return (NULL);
+
+	Nnode->n = number;
+	if (temp == NULL || temp->n >= number)
+	{
+		Nnode->next = temp;
+		*head = Nnode;
+		return (Nnode);
+	}
+	while (temp && temp->next && temp->next->n < number)
+	{
+		temp = temp->next;
+	}
+	Nnode->next = temp->next;
+	temp->next = Nnode;
+
+	return (Nnode);
+}
