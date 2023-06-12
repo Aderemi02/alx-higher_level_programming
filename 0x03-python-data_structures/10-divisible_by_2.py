ee4ebb763e596b94af1c463eafd2ef15e87a8f13
Aderemi <omoremilekun@gmail.com>
2023-06-12 15:08:35 -0700

    finding multiple of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newlist = []
+   for i in range(len(my_list)):
+       if my_list[i] % 2 == 0:
+           newlist.append(True)
+        newlist.append(False)
+    return newlist
