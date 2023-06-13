5890ae07203536aea04b0ff24532a5c14d8422b6
Aderemi <omoremilekun@gmail.com>
2023-06-13 12:21:18 -0700

    displaying arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) < 2:
+    if (len(argv) - 1) == 0:
         print("0: arguments.")
-    elif len(argv) < 3:
+    elif (len(argv) - 1) == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(len(argv) - 1))
-    for i in range(len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    for i in range(len(argv) - 1):
+        print("{}: {}".format(i + 1, argv[i + 1]))
