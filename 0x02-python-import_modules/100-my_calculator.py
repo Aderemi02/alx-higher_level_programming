2b17c8f89744c66ec808ad92c6f05860b740e87b
Aderemi <omoremilekun@gmail.com>
2023-06-14 15:28:14 -0700

    creating a simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     from sys import argv, exit
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
-    op = list(operator.keys())
     if len(argv) - 1 != 3:
         print("./100-my_calculator.py <a> operator <b>")
         exit(1)
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    op = list(operator.keys())
     if argv[2] not in op:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
@@ -13,4 +13,4 @@ if __name__ = "__main__":
     b = int(argv[3])
     c = argv[2]
     result = operator[c](a, b)
-    print("{:d} {:s} {:d} = {:d}".format(a, c, b, result))
+    print("{} {} {} = {}".format(a, c, b, result))
