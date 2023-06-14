a564758c764782c7e1be6a05ca541d27f6cd0ca9
Aderemi <omoremilekun@gmail.com>
2023-06-14 10:37:50 -0700

    number of keys in a dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,4 +2,4 @@
 def number_keys(a_dictionary):
     newlist = sorted(a_dictionary.keys())
     for i in newlist:
-        print("{}: {}".format(i, a_dictionary[i]))
+        print("{}: {}".format(i, a_dictionary.get(i)))
