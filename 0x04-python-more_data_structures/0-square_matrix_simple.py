9ea9e67e170088813d726a2b621e88b7a0e832ba
Aderemi <omoremilekun@gmail.com>
2023-06-14 09:31:43 -0700

    finding square of a list in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     result = matrix.copy()
-    for i in range (len(matrix)):
+    for i in range(len(matrix)):
         result[i] = list(map(lambda new: new * new, matrix[i]))
     return result
-
