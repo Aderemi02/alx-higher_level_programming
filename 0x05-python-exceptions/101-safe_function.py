bf273e49cb7d6f4025b200dbf19f49633eae9a7d
Aderemi <omoremilekun@gmail.com>
2023-06-26 12:41:58 -0700

    function executing function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
-    import sys.stderr.write as res
+    import sys.stderr as stderr
     try:
         result = fct(*args)
     except Exception as num:
-        res("Exception: {}\n".format(num))
+        stderr.write("Exception: {}\n".format(num))
         result = None
     return (result)
