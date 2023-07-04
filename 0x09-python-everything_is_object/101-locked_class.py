2faf19113e0bc626eaec1eebc65c5bbe2313e529
Aderemi <omoremilekun@gmail.com>
2023-07-04 08:05:56 -0700

    avoiding dynamically creation of attributes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a mmethod that prevent user from creating new instance attribute"""
+class LockedClass:
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """initializes the class"""
+        pass
