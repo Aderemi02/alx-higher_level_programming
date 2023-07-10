d3746e03b9b52ad3c3485af9d4bb7e0e5912fae7
Aderemi <omoremilekun@gmail.com>
2023-07-10 12:16:35 -0700

    validating value of the square with string rep

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+calculating the area of the square and also rep string
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    a square class
+    """
+
+    def __init__(self, size):
+        """
+        used to initialize the size
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        string representation
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
