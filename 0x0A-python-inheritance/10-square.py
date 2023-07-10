579d4d703bddca4cff60dc42938ea40c2d2dd9fc
Aderemi <omoremilekun@gmail.com>
2023-07-10 12:11:27 -0700

    validating value of the square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,7 +2,7 @@
 """
 calculating the area of the square and also rep string
 """
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -12,9 +12,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """
-        used to validate width and height
+        used to initialize the size
         """
 
         self.integer_validator("size", size)
-        super().__init__(self.size, self.__size)
+        super().__init__(size, size)
         self.__size = size
