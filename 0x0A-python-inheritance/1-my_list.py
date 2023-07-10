b603f4a62fa3f1eaf189aa737fdf978dfc02a470
Aderemi <omoremilekun@gmail.com>
2023-07-10 09:07:22 -0700

    sorting of a list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a function that inherit my from list
+"""
+
+
+def print_sorted(self):
+    """
+    a class that inherit mylist from list
+    """
+    new_sort = self.copy()
+    new_sort.sort()
+    print(new_sort)
