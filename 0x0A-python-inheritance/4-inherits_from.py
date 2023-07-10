90b4106b0c8816eb7c4c175cc65f8d3cac511b3b
Aderemi <omoremilekun@gmail.com>
2023-07-10 09:36:27 -0700

    checking for isinstance and instance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/use/bin/python3
+"""
+a function that returns:
+True if the object is an instance of a class
+that inherited (directly or indirectly) from
+the specified class;
+otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    a function that returns
+    True if the object oa an instance of a class
+    otheriwise
+    false
+    """
+    if type(obj) is a_class:
+        return False
+    elif isinstance(obj, a_class):
+        return True
+    else:
+        return False
