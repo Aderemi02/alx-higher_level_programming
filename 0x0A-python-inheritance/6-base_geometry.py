511eddeb562958fd6725ee8d8fe161d6c5b6492e
Aderemi <omoremilekun@gmail.com>
2023-07-10 09:50:24 -0700

    raising exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+writing a class based in 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
