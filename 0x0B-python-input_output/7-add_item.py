677c98e1c745afb98173665232bc380eea1dab50
Aderemi <omoremilekun@gmail.com>
2023-07-11 11:03:40 -0700

    adding all arg to a python list and saving to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    save_JSONfile = __import__("5-save_to_json_file").save_to_json_file
+    load_JSONfile = __import__("6-load_from_json_file").load_from_json_file
 
     try:
-        new_list = load_from_json_file("add_item.json")
+        new_list = load_JSONfile("add_item.json")
     except FileNotFoundError:
         new_list = []
-    new_list.extend(argv[1:])
-    save_to_json_file(new_list, "add_item.json")
+    for a in (argv[1:]):
+        new_list.append(a)
+    save_JSONfile(new_list, "add_item.json")
