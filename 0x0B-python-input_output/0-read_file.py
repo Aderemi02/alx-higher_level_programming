f06e5a8d8f186346ed972eab7adf4a88c4c1ea48
Aderemi <omoremilekun@gmail.com>
2023-07-11 10:03:47 -0700

    reading file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,12 +5,10 @@ and prints it to stdout
 """
 
 
-
 def read_file(filename=""):
     """
     a function that reads a textfile
     """
 
     with open(filename, 'r', encoding="UTF-8") as f:
-        f.read()
         print(f.read(), end="")
