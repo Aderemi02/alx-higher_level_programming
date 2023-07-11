685c807ca34e0e792d2b31ce35d32e1adcc62b6f
Aderemi <omoremilekun@gmail.com>
2023-07-11 10:08:07 -0700

    writing to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a function that writes a string to a text file (UTF8)
+and returns the number of characters written:
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    a function that writes to a textfile
+    """
+
+    with open(filename, 'w', encoding="UTF-8") as f:
+        return f.write(text)
