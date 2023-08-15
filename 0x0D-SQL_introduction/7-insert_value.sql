e95bc81df0476b3a5ff9c22347f3251a4e865439
Aderemi <omoremilekun@gmail.com>
2023-08-15 11:02:16 -0700

    inserting into tables in a databases

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- inserting a new row to a first_table from database hbtn_0c_0
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
+INSERT INTO first_table (id, name)
+VALUES ('89', 'Best School');
