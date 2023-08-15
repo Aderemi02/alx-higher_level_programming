9ee372d3c4ad4716c228b2f91fb0c462381a89de
Aderemi <omoremilekun@gmail.com>
2023-08-15 12:27:35 -0700

    lising all records in a table of a database except NULL ame

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- listing all records of the table
+-- dont list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument to the mysql command
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
