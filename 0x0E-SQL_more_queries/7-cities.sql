c8f204754caf9e0570b0296b724dea657aa75002
Aderemi <omoremilekun@gmail.com>
2023-08-16 14:57:12 -0700

    creating table cities with default value and unique id

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,7 +7,7 @@
 -- If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 USE htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR (256) NOT NULL,
 	FOREIGN KEY (state_id) REFEENCES states(id));
