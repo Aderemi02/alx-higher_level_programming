41708d5176eba86baf8957483c4245c8ac790747
Aderemi <omoremilekun@gmail.com>
2023-08-16 16:44:21 -0700

    comedy genre

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -10,5 +10,5 @@ JOIN tv_show_genres
 ON id = tv_show_genres.show_id
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genre.name = "Comedy"
+WHERE tv_genres.name = "Comedy"
 ORDER BY title ASC;
