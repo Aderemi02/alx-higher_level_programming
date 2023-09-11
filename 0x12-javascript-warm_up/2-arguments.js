f743ae298c817e9e2b3b3b2016031f420694d58a
Aderemi <omoremilekun@gmail.com>
2023-09-11 13:54:25 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,15 +4,10 @@ const myVar2 = 'Arguments found';
 const myVar3 = 'No argument';
 const argLen = process.argv.length;
 
-if (argLen === 2)
-{
+if (argLen === 2) {
 	console.log(myVar3);
-}
-else if (argLen === 3)
-{
+} else if (argLen === 3) {
 	console.log(myVar);
-}
-else
-{
+} else {
 	console.log(myVar2);
 }
