78f0fecc834238fbbb274629f7667ef867ea7126
Aderemi <omoremilekun@gmail.com>
2023-09-16 13:17:17 -0700

    listing states from a db

---
+++ venv/0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    a script that lists all states from the database hbtn_0e_0_usa
+    """
+
+    dbcon = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    dbcursor = dbcon.cursor()
+    dbcursor.execute("SELECT * FROM states")
+    row = dbcursor.fetchall()
+
+    for line in row:
+        print(line)
+    dbcursor.close()
+    dbcon.close()
