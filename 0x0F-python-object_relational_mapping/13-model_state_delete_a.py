dc63b998ae05a3008c6e3509de0b7ce6ea81693e
Aderemi <omoremilekun@gmail.com>
2023-09-17 16:27:20 -0700

    deleting all state with a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+a script that deletes a State object from the database hbtn_0e_6_usa
+"""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+    a script that deletes a State object “Louisiana”
+    from the database hbtn_0e_6_usa
+    """
+
+    engine = create_engine("mysql+mysqldb://{}:{}\
+            @localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    Sess = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    newSess = Sess()
+    states = newSess.query(State).order_by(
+            State.id.asc()).filter(State.name.like("%a%")).delete(
+                    synchronze_session=False)
+    newSess.commit()
+    newSess.close()
